778c23d1e949d8b2bf1f1c8c8ceaa61113fb550c
AdvikCode22 <94784477+AdvikCode22@users.noreply.github.com>
2023-01-16 17:33:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,15 +4,27 @@ drawn_sketch = ""
 counter = 0
 timer_check = ""
 
+function preload(){
+    model = ml5.imageClassifier("DoodleNet")
+}
+
 function setup(){
     myCanvas = createCanvas(350,350)
     myCanvas.center()
     background("white")
+    myCanvas.mouseReleased(classifyCanvas)
 }
 
-quick_draw_contents = ["airplane" , "ship" , "alarm_clock" , "apple" , "war_axe" , "banana" , 
-"basketball" , "camera" , "book" , "cake" , "cup" , "wall_clock" , "door" , "donut" , "glasses" , 
-"eye" , "candle" , "drum" , "lamp" , "foot" , "fork"]
+function classifyCanvas(){
+    model.classify(myCanvas,gotResults)
+}
+
+quick_draw_contents = ["flashlight","belt","mushroom","pond","strawberry","pineapple","sun","cow","ear",
+"bush","pliers","watermelon","apple","baseball","feather","leaf","lollipop","crown","ocean","horse",
+"mountain","mosquito","mug","hospital","castle","angel","underwear","traffic_light","cruise_ship","marker",
+"blueberry","flamingo","face","hockey_stick","bucket","campfire","asparagus","skateboard","door",
+"suitcase","skull","cloud","paint_can","hockey_puck","steak","house_plant","sleeping_bag","snowman",
+"arm","crayon","fan","star","rain","animal_migration","door"]
 
 random_number = Math.floor((Math.random() * quick_draw_contents.length) + 1)
 console.log(quick_draw_contents[random_number])
@@ -28,6 +40,11 @@ function updateCan(){
 }
 
 function draw(){
+    strokeWeight(13)
+    stroke("black")
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY)
+    }
     check_sketch()
     if(drawn_sketch == targetSketch){
         answer_holder = "set"
@@ -50,4 +67,27 @@ function check_sketch(){
         updateCan()
         counter = 0
     }
+}
+
+function clearCan(){
+    background("white")
+}
+
+function gotResults(error,results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("sketchName").innerHTML = "Your Sketch : " + results[0].label
+        document.getElementById("conf").innerHTML = "Accuracy : " + Math.round(results[0].confidence * 100) + "%"
+        drawn_sketch = results[0].label
+    }
+}
+
+function nextSketch(){
+    counter = 0
+    updateCan()
+    timer_check = ""
+    answer_holder = ""
 }
